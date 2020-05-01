40425ed8a50271f5c585758bb9b4805a9fc63c33
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-05-02 01:01:24 +0530

    Update and rename text_to_speech_v6.py to text_to_speech_changing_voice.py

--- speech<->text/text_to_speech_changing_voice.py
+++ speech<->text/text_to_speech_changing_voice.py
@@ -7,6 +7,21 @@ converter = pyttsx3.init()
 
 # Set properties before adding 
 # Things to say 
+voices = converter.getProperty('voices') 
+
+for voice in voices: 
+	# to get the info. about various voices in our PC 
+	print("Voice:") 
+	print("ID: %s" %voice.id) 
+	print("Name: %s" %voice.name) 
+	print("Age: %s" %voice.age) 
+	print("Gender: %s" %voice.gender) 
+	print("Languages Known: %s" %voice.languages) 
+
+voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+  
+# Use female voice 
+converter.setProperty('voice', voice_id) 
 
 # Sets speed percent 
 # Can be more than 100 
